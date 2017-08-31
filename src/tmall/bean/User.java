454171469e76fc4e8991d6358d579bf9122f228c
heyouqiang_i <heyouqiang_i@didichuxing.com>
2017-08-31 13:52:34 +0800

    添加User类，版本控制尝试

---
+++ src/tmall/bean/User.java
@@ -0,0 +1,31 @@
+package tmall.bean;
+
+/**
+ * Created by mountain on 2017/8/31.
+ */
+public class User {
+    private String password;
+    private String name;
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getAnonymousName
+
+}
